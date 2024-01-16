f17985b37d5cd3d79cfbf1f6b42f403f6e580c44
Ismailduman <issmailduman@gmail.com>
2024-01-16 11:44:43 +0100

    runners and hooks

---
+++ src/test/java/com/library/step_defs/Hooks.java
@@ -0,0 +1,52 @@
+package com.library.step_definitions;
+
+
+
+
+import com.library.utility.ConfigurationReader;
+import com.library.utility.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.restassured.RestAssured;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.time.Duration;
+
+
+public class Hooks {
+
+    @Before("@ui")
+    public void setUp(){
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        Driver.getDriver().manage().window().maximize();
+        Driver.getDriver().get(ConfigurationReader.getProperty("library_url"));
+
+    }
+
+    @After("@ui")
+    public void tearDown(Scenario scenario){
+        if(scenario.isFailed()){
+            final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
+
+        Driver.closeDriver();
+
+    }
+
+    @Before("@db")
+    public void setUpDB(){
+        System.out.println("Connecting to database...");
+      com.cydeo.utility.DB_Util.createConnection();
+    }
+
+    @After("@db")
+    public void tearDownDB(){
+        System.out.println("close database connection...");
+        com.cydeo.utility.DB_Util.destroy();
+    }
+
+
+}
